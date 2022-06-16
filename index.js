4b466f61100d4cfdddef1557e27358fa37f41077
fancyjimmy <jimmyfan04@gmail.com>
2022-06-16 23:29:48 +0200

    initial

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("1");
+import express from "express";
+import dotenv from "dotenv";
+import {Low, JSONFile} from "lowdb";
+import {join} from "path";
+
+dotenv.config();
+
+let port = process.env.PORT || 3000;
+let app = express();
+
+const adapter = new JSONFile(join(__dirname, process.env.DATAPATH));
+const db = new Low( adapter);
+
+
+
+app.listen(port, () => {
+    console.log("listening on port http://localhost:3000");
+})
